862e7d44b6b945f9ee53b5dceb44f833c80a7b38
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-08-04 19:09:23 -0500

    update

---
+++ src/main/java/com/ulasalle/gestorcitasconsultaclinicaspring/GestorCitasConsultaClinicaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ulasalle.gestorcitasconsultaclinicaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCitasConsultaClinicaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorCitasConsultaClinicaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ulasalle/gestorcitasconsultaclinicaspring/GestorCitasConsultaClinicaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasalle.gestorcitasconsultaclinicaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCitasConsultaClinicaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
